7860b84e347961512ae9501e6ce25726903a7fdc
Rufio Dinoto(Musk) <244thanva@gmail.com>
2026-02-17 17:57:34 +0700

    Add macro recording and saving functionality

--- server/recorder.js
+++ server/recorder.js
@@ -1,1 +1,14 @@
+let macros = [];
 
+function recordAction(action){
+  macros.push({...action, timestamp: Date.now()});
+}
+
+function saveMacro(){
+  const fs = require("fs");
+  const {v4: uuid} = require("uuid");
+  fs.writeFileSync(`./server/macro-${uuid()}.json`, JSON.stringify(macros,null,2));
+  macros = [];
+}
+
+module.exports = {recordAction, saveMacro};
